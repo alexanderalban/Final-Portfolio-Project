ace5de6e79cd80983b8165b8b39f235a8cb71c35
rockman4417 <67634648+rockman4417@users.noreply.github.com>
2020-10-14 10:18:43 -0500

    initial Landing Page

---
+++ Landing.js
@@ -0,0 +1,22 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.background = "rgb(37, 37, 37)";
+  } else {
+    document.getElementById("navbar").style.opacity = "rgb(59, 59, 59)";
+  }
+}
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
